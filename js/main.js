d7099b691d9047566ef226408b44599c46bf9c8a
AkimovAlex <59553439+AkimovAlex@users.noreply.github.com>
2020-01-29 10:44:02 +0200

    q

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+// multislider
+$('#exampleSlider').multislider({
+        interval: 0,
+        slideAll: true,
+        duration: 1500
+    });
