1d486bd51ee8078027620682e6993372367ee5f4
AkimovAlex <59553439+AkimovAlex@users.noreply.github.com>
2020-04-30 10:27:10 +0300

    1

--- Sparta/js/main.js
+++ Sparta/js/main.js
@@ -1,1 +1,11 @@
 document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+// slider
+$(document).ready(function() {
+    $('.slider-block').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        // centerMode: true,
+        dots: true,
+    });
+});
