79a8fcb4ea7e4d68bf18436f098645b42691a89c
AkimovAlex <59553439+AkimovAlex@users.noreply.github.com>
2020-12-03 13:57:30 +0200

    1

--- my-test/js/main.js
+++ my-test/js/main.js
@@ -264,3 +264,25 @@ var objectFitVideos = function(videos) {
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
     module.exports = objectFitVideos;
+// up
+$(function() {
+    // при нажатии на кнопку scrollup
+    $('.scrollup').click(function() {
+        // переместиться в верхнюю часть страницы
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop() > 200) {
+        // то сделать кнопку scrollup видимой
+        $('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+        $('.scrollup').fadeOut();
+    }
+});
