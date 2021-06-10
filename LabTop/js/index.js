9ff469bdeffcb0e1be2b7cf56f7db743505af054
AkimovAlex <59553439+AkimovAlex@users.noreply.github.com>
2021-06-10 18:52:05 +0300

    1

---
+++ LabTop/js/index.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    $('#animatedElement').each(function(){
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("slideUp");
+        }
+    });
+});
+window.onload = function() { document.body.innerHTML = document.body.innerHTML.replace(/\u2029/g, ''); }
